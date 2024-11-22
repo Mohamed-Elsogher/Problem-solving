ded8f0da68394db31457b874803106770c22d855
Mohamed Abdelhady Mohamed Elsogher <155487881+Mohamed-Elsogher@users.noreply.github.com>
2024-11-22 21:57:40 +0200

    Delete Problem Solving/Problem-Solving-CodeForces directory

--- Problem Solving/Problem-Solving-CodeForces/Word Capitalization.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
- 
-public class Main{
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        System.out.println(str.toUpperCase().charAt(0)+str.substring(1));
-    }
