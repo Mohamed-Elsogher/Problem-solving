c6da1705f6d95ba2dcf23b43d719880dbe2ec664
Mohamed Abdelhady Mohamed Elsogher <155487881+Mohamed-Elsogher@users.noreply.github.com>
2024-12-15 23:12:16 +0200

    Create 2044A. Easy Problem.cpp

---
+++ CodeForces/2044A. Easy Problem.cpp
@@ -0,0 +1,32 @@
+/**
+ *    author:  Hady17
+ *
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define arr array
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << n - 1 << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;
+    cin >> tt;
+
+    while (tt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
