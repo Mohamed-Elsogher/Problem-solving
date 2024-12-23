944121bea5aaee92154c1b07fd03b524bcac70a9
Mohamed Abdelhady Mohamed Elsogher <155487881+Mohamed-Elsogher@users.noreply.github.com>
2024-12-23 18:16:38 +0200

    Create Find Pivot Index.cpp

---
+++ LeetCode/Find Pivot Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+         if(nums.size() == 0) return - 1;
+        int leftsum =0 ; int rightsum = 0 ;
+        for(int i : nums){
+            rightsum += i ;
+        }
+        for(int i = 0 ; i < nums.size() ; i++){
+            rightsum -= nums[i];
+            if(rightsum == leftsum) return i ;
+            leftsum += nums[i];
+        }
+        return -1 ;
+    }
+};
