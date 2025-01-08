b8ed970ec94a0b143e858c4a2820db5678f11088
Mohamed Abdelhady Mohamed Elsogher <155487881+Mohamed-Elsogher@users.noreply.github.com>
2025-01-09 00:39:49 +0200

    Linked List - Concept.cpp

---
+++ CodeChef/LinkedList/Linked List - Concept.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Create a struct with two values
+struct Node {
+    int value; 
+    Node* next ;
+    Node (int val): value(val) , next(nullptr){}
+};
+
+int main() {
+    // Creating head of the Linked list
+    Node* head = new Node(1);
+    cout << "The value at head is " << head -> value << '\n';
+}
