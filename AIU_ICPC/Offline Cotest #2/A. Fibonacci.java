6af00ea11ffad2c24860978491346362472c0f8d
Mohamed Abdelhady Mohamed Elsogher <155487881+Mohamed-Elsogher@users.noreply.github.com>
2025-03-09 09:18:07 +0200

    Update A. Fibonacci.java

--- AIU_ICPC/Offline Cotest #2/A. Fibonacci.java
+++ AIU_ICPC/Offline Cotest #2/A. Fibonacci.java
@@ -1,5 +1,6 @@
 import java.util.*;
-// In this question count start from 1 not from 0 
+// In this question count start from 1 not from 0  
+// Time Complexity: O(2^n)  -> N (1 ≤ N ≤ 30) so Will not have time limit but if N was bigger we will use dp memoization 
 // Fibonacci -> 0 1 1 3 5 8 13 
 /*
 f{1} = 0  // index 1
@@ -12,6 +13,20 @@ f{4} = 2 // index 4
 
 f{5} = 3 // index 5
 ....... so on
+Recursion:
+F(n)
+├── F(n-1)
+│   ├── F(n-2)
+│   │   ├── F(n-3)
+│   │   │   ├── ...
+│   │   │   └── ...
+│   │   └── ...
+│   └── ...
+└── F(n-2)
+    ├── F(n-3)
+    │   ├── ...
+    │   └── ...
+    └── ...
 */
 public class Main {
     public static long Fibonacci(long num) {
